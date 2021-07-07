1fd75557884c28407598a8c96ad37321215079ea
wonholee24 <zlfeptm@naver.com>
2021-07-07 23:59:21 +0900

    test

--- pythonTmp1.py
+++ pythonTmp1.py
@@ -1,1 +1,25 @@
-print("print(\"Hello\\nWorld\")")
+#print("print(\"Hello\\nWorld\")")
+
+# c1, c2 = input().split()
+# print(c2, c1, end=" ")
+
+# tmp = input()
+# print(tmp, tmp, tmp)
+
+# a, b = input().split(sep=":")
+# print(a, ":", b, sep="")
+
+# a, b, c = input().split(sep=".")
+# print(c, "-", b, "-", a, sep="")
+
+# a, b = input().split(sep="-")
+# print(a,b,sep="")
+
+# list = input()
+# for i in range(len(list)):
+#     print(list[i])
+
+# list = input()
+# print(list[0:2],end=" ")
+# print(list[2:4],end=" ")
+# print(list[4:6],end=" ")
